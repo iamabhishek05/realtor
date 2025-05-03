e177d7d29cdcabdad01856994a920179d8dd1399
iamabhishek05 <abhishektiwari290699@gmail.com>
2025-05-03 17:04:35 +0530

    Completed sign up page functionality

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
+
 
 mongoose.connect(process.env.DB_URI)
 .then( () =>{
@@ -14,8 +16,26 @@ mongoose.connect(process.env.DB_URI)
 
 const app = express();
 
+app.use(express.json());
+
 
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
+})
+
+app.use('/api/auth',authRouter);
+
+
+// Error handling middleware
+// This middleware will catch any errors thrown in the routes
+// and send a response with the error message and status code
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        status: statusCode,
+        message: message,
+    })
 })
